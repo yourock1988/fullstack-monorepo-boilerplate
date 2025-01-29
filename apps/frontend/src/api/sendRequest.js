1b653f4aa3d76189f05354f9dcd3931df088c80e
YouROCK <yourock88@meta.ua>
2025-01-30 00:31:15 +0200

    Implement students api

---
+++ apps/frontend/src/api/sendRequest.js
@@ -0,0 +1,22 @@
+async function sendRequest(url, method = 'GET', payload = null) {
+  const request = {
+    method,
+  }
+
+  if (payload) {
+    request.headers = {
+      'Content-Type': 'application/json',
+    }
+    request.body = JSON.stringify(payload)
+  }
+
+  const resp = await fetch(url, request)
+  const text = await resp.text()
+  if (text) {
+    const json = JSON.parse(text)
+    return json
+  }
+  return {}
+}
+
+export default sendRequest
