14dd8f605a384a80a4e1b8618881bae80dea3ec5
YouROCK <yourock88@meta.ua>
2025-01-30 00:32:03 +0200

    Implement students store

--- apps/frontend/src/store/index.js
+++ apps/frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex'
 
 import example from './example'
+import students from './students'
 
 export default createStore({
   modules: {
     example,
+    students,
   },
 })
