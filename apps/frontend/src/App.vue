6fe7973ff12a08881733a9ed24eeee0150b70877
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-02-16 15:30:06 +0200

    Implemented: Vue component PageCatalog

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -7,17 +7,5 @@ export default {
 </script>
 
 <template>
-  <h1>Vue Router</h1>
   <PageCatalog></PageCatalog>
-  <!-- <p><strong>Текущий путь маршрута: </strong> {{ $route.fullPath }}</p> -->
-  <!-- <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
-    <span> | </span>
-    <RouterLink to="/students">Go to Students</RouterLink>
-    <span> | </span>
-    <RouterLink to="/products">Go to Products</RouterLink>
-  </nav>
-  <main>
-    <RouterView />
-  </main> -->
 </template>
