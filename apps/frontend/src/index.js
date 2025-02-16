02fac82fd05bb1790f91693f38e3f986e0723c45
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-02-16 15:29:25 +0200

    Chore: import css styles in index.js

--- apps/frontend/src/index.js
+++ apps/frontend/src/index.js
@@ -3,7 +3,14 @@ import * as Vue from 'vue'
 // import router from '@/router'
 
 import App from '@/App.vue'
-// import '@/assets/css/style.css'
+
+import '@/assets/css/reset.css'
+import '@/assets/css/vars.css'
+import '@/assets/css/layout.css'
+import '@/assets/css/appearance.css'
+import '@/assets/css/media.css'
+
+import '@fortawesome/fontawesome-free/css/all.css'
 
 const app = Vue.createApp(App)
 
