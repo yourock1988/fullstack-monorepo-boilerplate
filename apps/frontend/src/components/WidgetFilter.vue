d32974bb0d6243df0570395649e463a5dfc5c12c
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-02-16 16:44:05 +0200

    Extracted: component WidgetFilter.vue

---
+++ apps/frontend/src/components/WidgetFilter.vue
@@ -0,0 +1,239 @@
+<script>
+export default {
+  //
+}
+</script>
+
+<template>
+  <aside class="catalog-filter">
+    <h3>Фильтры</h3>
+    <dl class="filter-attribute">
+      <dt><h4>Видеокарта</h4></dt>
+      <dd>
+        <ul>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_01" type="checkbox" />
+              <label for="chk_01">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 3060 <sup>(10)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_02" type="checkbox" />
+              <label for="chk_02">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 3060 Ti <sup>(7)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_03" type="checkbox" />
+              <label for="chk_03">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 4070 <sup>(15)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_04" type="checkbox" />
+              <label for="chk_04">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 5070 <sup>(3)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_05" type="checkbox" />
+              <label for="chk_05">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 5070 Ti <sup>(23)</sup></span>
+              </label>
+            </div>
+          </li>
+        </ul>
+      </dd>
+    </dl>
+    <dl class="filter-attribute">
+      <dt><h4>Видеокарта</h4></dt>
+      <dd>
+        <ul>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_01" type="checkbox" />
+              <label for="chk_01">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 3060 <sup>(10)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_02" type="checkbox" />
+              <label for="chk_02">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 3060 Ti <sup>(7)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_03" type="checkbox" />
+              <label for="chk_03">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 4070 <sup>(15)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_04" type="checkbox" />
+              <label for="chk_04">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 5070 <sup>(3)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_05" type="checkbox" />
+              <label for="chk_05">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 5070 Ti <sup>(23)</sup></span>
+              </label>
+            </div>
+          </li>
+        </ul>
+      </dd>
+    </dl>
+    <dl class="filter-attribute">
+      <dt><h4>Видеокарта</h4></dt>
+      <dd>
+        <ul>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_01" type="checkbox" />
+              <label for="chk_01">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 3060 <sup>(10)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_02" type="checkbox" />
+              <label for="chk_02">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 3060 Ti <sup>(7)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_03" type="checkbox" />
+              <label for="chk_03">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 4070 <sup>(15)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_04" type="checkbox" />
+              <label for="chk_04">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 5070 <sup>(3)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_05" type="checkbox" />
+              <label for="chk_05">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 5070 Ti <sup>(23)</sup></span>
+              </label>
+            </div>
+          </li>
+        </ul>
+      </dd>
+    </dl>
+    <dl class="filter-attribute">
+      <dt><h4>Видеокарта</h4></dt>
+      <dd>
+        <ul>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_01" type="checkbox" />
+              <label for="chk_01">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 3060 <sup>(10)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_02" type="checkbox" />
+              <label for="chk_02">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 3060 Ti <sup>(7)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_03" type="checkbox" />
+              <label for="chk_03">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 4070 <sup>(15)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_04" type="checkbox" />
+              <label for="chk_04">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 5070 <sup>(3)</sup></span>
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="wrap-checkbox-fa">
+              <input id="chk_05" type="checkbox" />
+              <label for="chk_05">
+                <i class="fa-solid fa-square-check"></i>
+                <i class="fa-solid fa-square"></i>
+                <span>GeForce RTX 5070 Ti <sup>(23)</sup></span>
+              </label>
+            </div>
+          </li>
+        </ul>
+      </dd>
+    </dl>
+  </aside>
+</template>
