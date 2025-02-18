f9b155274bdd8df54ca4f64aa9efe7744efaf2c1
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-02-18 12:19:39 +0200

    Added: logic to SelectSearchQuery.vue

--- apps/frontend/src/components/SelectSearchQuery.vue
+++ apps/frontend/src/components/SelectSearchQuery.vue
@@ -1,3 +1,37 @@
+<script>
+export default {
+  emits: ['search-query-changed'],
+
+  data() {
+    return {
+      searchQuery: '',
+      isUpdated: false,
+    }
+  },
+
+  watch: {
+    searchQuery(newValue) {
+      this.isUpdated = true
+      if (newValue === '') {
+        this.$emit('search-query-changed', newValue)
+      }
+    },
+  },
+
+  methods: {
+    changeSearchQuery() {
+      if (this.isUpdated && this.searchQuery !== '') {
+        this.$emit('search-query-changed', this.searchQuery)
+        this.isUpdated = false
+      }
+    },
+  },
+}
+</script>
+
 <template>
-  <div class="wrap-search"><input type="search" /><button>Найти</button></div>
+  <div class="wrap-search">
+    <input v-model.trim="searchQuery" type="search" />
+    <button @click="changeSearchQuery">Найти</button>
+  </div>
 </template>
