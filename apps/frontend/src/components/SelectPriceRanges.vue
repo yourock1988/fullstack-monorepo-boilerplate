c13a3030adf448520db6a6a62de4de6d5275d7e2
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-02-18 14:53:04 +0200

    Implemented: SelectPriceRanges logic

--- apps/frontend/src/components/SelectPriceRanges.vue
+++ apps/frontend/src/components/SelectPriceRanges.vue
@@ -1,27 +1,41 @@
+<script>
+export default {
+  data() {
+    return {
+      min: 3000,
+      max: 9000,
+      from: 4000,
+      to: 8000,
+    }
+  },
+
+  watch: {
+    from(newValue) {
+      if (newValue > this.to) this.to = newValue
+    },
+    to(newValue) {
+      if (newValue < this.from) this.from = newValue
+    },
+  },
+}
+</script>
+
 <template>
   <div class="price-ranges">
     <h3>Цена</h3>
     <div class="wrap-input-range">
-      <label for="price-from">От: <span>3000</span></label>
-      <input
-        id="price-from"
-        type="range"
-        name="price-from"
-        min="3000"
-        max="9000"
-        value="3000"
-      />
+      <label for="priceFrom">
+        <span>От: </span>
+        <b>{{ from }}</b>
+      </label>
+      <input id="priceFrom" v-model="from" type="range" :min="min" :max="max" />
     </div>
     <div class="wrap-input-range">
-      <label for="price-to">До: <span>9000</span></label>
-      <input
-        id="price-to"
-        type="range"
-        name="price-to"
-        min="3000"
-        max="9000"
-        value="9000"
-      />
+      <label for="priceTo">
+        <span>До: </span>
+        <b>{{ to }}</b>
+      </label>
+      <input id="priceTo" v-model="to" type="range" :min="min" :max="max" />
     </div>
   </div>
 </template>
