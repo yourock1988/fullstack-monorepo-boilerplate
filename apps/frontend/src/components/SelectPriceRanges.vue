ffcca3bc91ccc54da7d89d529f5e05312b6adf47
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-02-18 14:28:15 +0200

    Extracted: component SelectPriceRanges.vue

---
+++ apps/frontend/src/components/SelectPriceRanges.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="price-ranges">
+    <h3>Цена</h3>
+    <div class="wrap-input-range">
+      <label for="price-from">От: <span>3000</span></label>
+      <input
+        id="price-from"
+        type="range"
+        name="price-from"
+        min="3000"
+        max="9000"
+        value="3000"
+      />
+    </div>
+    <div class="wrap-input-range">
+      <label for="price-to">До: <span>9000</span></label>
+      <input
+        id="price-to"
+        type="range"
+        name="price-to"
+        min="3000"
+        max="9000"
+        value="9000"
+      />
+    </div>
+  </div>
+</template>
--- apps/frontend/src/components/WidgetFilter.vue
+++ apps/frontend/src/components/WidgetFilter.vue
@@ -1,36 +1,15 @@
 <script>
+import SelectPriceRanges from './SelectPriceRanges.vue'
+
 export default {
-  //
+  components: { SelectPriceRanges },
 }
 </script>
 
 <template>
   <aside class="catalog-filter">
-    <div class="price-ranges">
-      <h3>Цена</h3>
-      <div class="wrap-input-range">
-        <label for="price-from">От: <span>3000</span></label>
-        <input
-          id="price-from"
-          type="range"
-          name="price-from"
-          min="3000"
-          max="9000"
-          value="3000"
-        />
-      </div>
-      <div class="wrap-input-range">
-        <label for="price-to">До: <span>9000</span></label>
-        <input
-          id="price-to"
-          type="range"
-          name="price-to"
-          min="3000"
-          max="9000"
-          value="9000"
-        />
-      </div>
-    </div>
+    <SelectPriceRanges />
+
     <div class="filter-attributes">
       <h3>Фильтры</h3>
       <dl class="filter-attribute">
