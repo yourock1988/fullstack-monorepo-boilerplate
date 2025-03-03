90b0b37f32ad7e2b8d9ddb51cd1e2139ab8c4bc5
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-03-03 03:54:17 +0200

    Extracted: dumb component SelectListType.vue

---
+++ apps/frontend/src/components/SelectListType.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrap-picker-tile-style">
+    <div id="elPickerTileStyle" class="picker-tile-style">
+      <input id="s-grid" type="radio" name="tile-style" checked />
+      <input id="s-table" type="radio" name="tile-style" />
+      <input id="s-list" type="radio" name="tile-style" />
+      <label for="s-grid">
+        <i class="fa-brands fa-microsoft"></i>
+      </label>
+      <label for="s-table">
+        <i class="fa-solid fa-table"></i>
+      </label>
+      <label for="s-list">
+        <i class="fa-solid fa-water"></i>
+      </label>
+    </div>
+  </div>
+</template>
--- apps/frontend/src/components/VerboseFiltration.vue
+++ apps/frontend/src/components/VerboseFiltration.vue
@@ -3,10 +3,12 @@ import SelectSortingType from './SelectSortingType.vue'
 import SelectPageSize from './SelectPageSize.vue'
 import ShowFiltratedCount from './ShowFiltratedCount.vue'
 import ResetAttributes from './ResetAttributes.vue'
+import SelectListType from './SelectListType.vue'
 
 export default {
   components: {
     SelectSortingType,
+    SelectListType,
     SelectPageSize,
     ShowFiltratedCount,
     ResetAttributes,
@@ -53,21 +55,6 @@ export default {
 
     <SelectPageSize @page-size-changed="changePageSize" />
 
-    <div class="wrap-picker-tile-style">
-      <div id="elPickerTileStyle" class="picker-tile-style">
-        <input id="s-grid" type="radio" name="tile-style" checked />
-        <input id="s-table" type="radio" name="tile-style" />
-        <input id="s-list" type="radio" name="tile-style" />
-        <label for="s-grid">
-          <i class="fa-brands fa-microsoft"></i>
-        </label>
-        <label for="s-table">
-          <i class="fa-solid fa-table"></i>
-        </label>
-        <label for="s-list">
-          <i class="fa-solid fa-water"></i>
-        </label>
-      </div>
-    </div>
+    <SelectListType />
   </div>
 </template>
