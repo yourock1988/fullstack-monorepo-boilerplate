4842d83255b8ff88847b96f38345bff7c0646a7c
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-03-13 00:43:53 +0200

    Implemented: store cart

--- apps/frontend/src/store/index.js
+++ apps/frontend/src/store/index.js
@@ -3,9 +3,11 @@ import { createStore } from 'vuex'
 import product from './product'
 import products from './products'
 import watchedProducts from './watchedProducts'
+import cart from './cart'
 
 export default createStore({
   modules: {
+    cart,
     product,
     products,
     watchedProducts,
