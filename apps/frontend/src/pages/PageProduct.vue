c4a44504412fb0d5d7770b1c21e8d1ab0ad35ad0
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-03-12 21:27:39 +0200

    Removed: unnecessary div

--- apps/frontend/src/pages/PageProduct.vue
+++ apps/frontend/src/pages/PageProduct.vue
@@ -57,68 +57,64 @@ export default {
 </script>
 
 <template>
-  <div>
-    <div class="layout-main">
-      <div class="layout-centralize">
-        <main>
-          <CategoryNavigation />
-          <div class="product-card">
-            <div class="slider-photos">
-              <div class="photo">
-                <img :src="mainPhoto" alt="" />
-              </div>
-              <div class="photos">
-                <div v-for="(photo, idx) of photos" :key="idx">
-                  <img :src="photo" alt="" height="50" />
-                </div>
-              </div>
+  <div class="layout-main">
+    <div class="layout-centralize">
+      <main>
+        <CategoryNavigation />
+        <div class="product-card">
+          <div class="slider-photos">
+            <div class="photo">
+              <img :src="mainPhoto" alt="" />
             </div>
-            <div class="about-text">
-              <div class="caption">
-                <h2>{{ product?.caption }}</h2>
-              </div>
-              <div class="feedback">
-                <span> Оставить отзыв </span>
-              </div>
-              <div class="available">
-                <span> Есть в наличии </span>
-              </div>
-              <div class="price">
-                <span> {{ product?.priceUah }}₴ </span>
-              </div>
-              <div class="buy">
-                <button>Купить</button>
-              </div>
-              <div class="delivery">
-                <span> Доставка </span>
-                <div>
-                  <span> в <b> Киев </b> </span>
-                  <div>
-                    <span> Курьером </span>
-                  </div>
-                  <div>
-                    <span> Самовывоз </span>
-                  </div>
-                </div>
-              </div>
-              <div class="payment">
-                <span> Оплата </span>
-                <p>
-                  Картой онлайн, Оплата при получении товара, Оплата картой в
-                  отделении, Google Pay
-                </p>
-              </div>
-              <div class="varranty">
-                <span> Гарантия </span>
-                <p>
-                  Гарантия. 38 месяцев Обмен/возврат товара в течение 14 дней
-                </p>
+            <div class="photos">
+              <div v-for="(photo, idx) of photos" :key="idx">
+                <img :src="photo" alt="" height="50" />
               </div>
             </div>
           </div>
-        </main>
-        <ShowWatchedProducts :products="watchedProducts" />
-      </div>
+          <div class="about-text">
+            <div class="caption">
+              <h2>{{ product?.caption }}</h2>
+            </div>
+            <div class="feedback">
+              <span> Оставить отзыв </span>
+            </div>
+            <div class="available">
+              <span> Есть в наличии </span>
+            </div>
+            <div class="price">
+              <span> {{ product?.priceUah }}₴ </span>
+            </div>
+            <div class="buy">
+              <button>Купить</button>
+            </div>
+            <div class="delivery">
+              <span> Доставка </span>
+              <div>
+                <span> в <b> Киев </b> </span>
+                <div>
+                  <span> Курьером </span>
+                </div>
+                <div>
+                  <span> Самовывоз </span>
+                </div>
+              </div>
+            </div>
+            <div class="payment">
+              <span> Оплата </span>
+              <p>
+                Картой онлайн, Оплата при получении товара, Оплата картой в
+                отделении, Google Pay
+              </p>
+            </div>
+            <div class="varranty">
+              <span> Гарантия </span>
+              <p>Гарантия. 38 месяцев Обмен/возврат товара в течение 14 дней</p>
+            </div>
+          </div>
+        </div>
+      </main>
+      <ShowWatchedProducts :products="watchedProducts" />
     </div>
   </div>
 </template>
