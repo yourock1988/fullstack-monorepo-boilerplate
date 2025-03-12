e393683369c95b2cdd3fe2f742e17482f10204d9
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-03-13 00:44:18 +0200

    Added: logic to PageCart.vue

--- apps/frontend/src/pages/PageCart.vue
+++ apps/frontend/src/pages/PageCart.vue
@@ -1,7 +1,20 @@
 <script>
 import '@/assets/css/cart.css'
+import CartItem from '~/CartItem.vue'
 
-export default {}
+export default {
+  components: { CartItem },
+
+  computed: {
+    cartProducts() {
+      return this.$store.getters['cart/getCartProducts']
+    },
+  },
+
+  mounted() {
+    this.$store.dispatch('cart/readCartsProducts')
+  },
+}
 </script>
 
 <template>
@@ -9,41 +22,11 @@ export default {}
     <h1>Ваша корзина</h1>
 
     <div class="cart-items">
-      <div class="cart-item">
-        <input id="show-menu-01" type="checkbox" />
-        <div class="top">
-          <div class="item-image">
-            <img src="../assets/img/computer.webp" alt="Товар 1" />
-          </div>
-          <div class="item-details">
-            <h2>Офисный ПК Solaris-I</h2>
-            <p>Есть в наличии</p>
-          </div>
-          <div class="item-menu"><label for="show-menu-01">:</label></div>
-        </div>
-
-        <div class="bottom">
-          <div class="item-counter">
-            <button>-</button>
-            <input type="text" size="2" value="1" />
-            <button>+</button>
-          </div>
-          <div class="item-price">1000₴</div>
-        </div>
-
-        <div class="context-menu">
-          <ul>
-            <li>
-              <i>🎉</i>
-              <span>Добавить в желания</span>
-            </li>
-            <li>
-              <i>🧨</i>
-              <span>Удалить</span>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <CartItem
+        v-for="cartProduct of cartProducts"
+        :key="cartProduct.id"
+        :cart-product="cartProduct"
+      />
     </div>
 
     <div class="confirmation">
