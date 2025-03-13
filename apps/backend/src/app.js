7b15dd738ae4c70d6982d542bd0a78166d815874
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-03-13 06:01:13 +0200

    Implemented: .env in backend

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -5,6 +5,8 @@ const app = express()
 const PORT = 8801
 const APP_NAME = 'boilerplate'
 
+global.console.log(process.env.APP_BE_COMMENT)
+
 app.use(routes)
 
 app.listen(PORT, () => global.console.log(`${APP_NAME} start st PORT: ${PORT}`))
