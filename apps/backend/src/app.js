769d521f058de3e2cd1abf571e5d59f1a453261e
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-03-20 12:53:02 +0200

    Added: check app argv

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = require('./routes')
 
 const app = express()
-const PORT = process.env.APP_BE_PORT
+const PORT = process.env.APP_BE_PORT || 5000
 const APP_NAME = 'Boilerplate'
 
 global.console.log(process.env.APP_BE_COMMENT)
@@ -10,3 +10,8 @@ global.console.log(process.env.APP_BE_COMMENT)
 app.use(routes)
 
 app.listen(PORT, () => global.console.log(`${APP_NAME} start at PORT: ${PORT}`))
+
+if (process.argv[2] === '--check') {
+  global.console.log('!!!STARTED_IN_CHECK_MODE!!!')
+  setTimeout(() => process.exit(0), 7000)
+}
